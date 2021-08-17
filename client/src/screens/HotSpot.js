57df0fde14ee06d85a518eceabe6200349df1590
JangByeongHui <jmkabc31@ajou.ac.kr>
2021-08-17 23:31:14 +0900

    탭 바 구현

---
+++ client/src/screens/HotSpot.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const HotSpot=()=>{
+
+    return(
+        <View>
+            <Text>핫스팟 구현할 페이지</Text>
+        </View>
+    );
+
+};
+
+export default HotSpot;
---
+++ client/src/screens/maps.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const maps=()=>{
+
+    return(
+        <View>
+            <Text>지도가 들어갈 탭 위치</Text>
+        </View>
+    );
+
+};
+
+export default maps;
