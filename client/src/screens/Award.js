b19ac76dce05af9dc9ff9921acc36e7bf9760437
JangByeongHui <jmkabc31@ajou.ac.kr>
2021-08-18 23:11:26 +0900

    feat-Tab Bar 아이콘 변경 및 스크린 추가

--- client/src/screens/Award.js
+++ client/src/screens/Award.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const ChannelList = () => {
+const Award = () => {
   return (
     <View>
-      <Text>채널 구현할 페이지</Text>
+      <Text>보상 구현 페이지</Text>
     </View>
   );
 };
 
-export default ChannelList;
+export default Award;
