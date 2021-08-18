ccccd4e5aeef65b3292256cd048a3f4a535b49e7
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-08-18 13:22:30 +0900

    Add alias setting

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@components": "./components",
+            "@contexts": "./contexts",
+            "@navigations": "./navigations",
+            "@screens": "./screens",
+            "@utils": "./utils",
+          },
+        },
+      ],
+    ],
   };
 };
