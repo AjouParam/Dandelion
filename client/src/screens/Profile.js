f7fc94a2cae75185c6648af3056fc63f59ffef14
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-08-24 02:36:54 +0900

    fix : moidfy profile tab rendering error

--- client/src/screens/Profile.js
+++ client/src/screens/Profile.js
@@ -16,25 +16,25 @@ const Container = styled.View`
 
 // const Profile = () => {
 //   // const { dispatch } = useContext(UserContext);
-//   const { spinner } = useContext(ProgressContext);
-//   const theme = useContext(ThemeContext);
+// const { spinner } = useContext(ProgressContext);
+// const theme = useContext(ThemeContext);
 
-//   const [email, setEmail] = useRecoilState(userState.emailState);
-//   const [uid, setUid] = useRecoilState(userState.uidState);
+// const [email, setEmail] = useRecoilState(userState.emailState);
+// const [uid, setUid] = useRecoilState(userState.uidState);
 
-//   const name = '테스터';
+// const name = '테스터';
 
-//   const _handleLogoutButtonPress = async () => {
-//     try {
-//       spinner.start();
-//       await logout();
-//     } catch (e) {
-//       console.log('[Profile] logout: ', e.message);
-//     } finally {
-//       dispatch({});
-//       spinner.stop();
-//     }
-//   };
+// const _handleLogoutButtonPress = async () => {
+//   try {
+//     spinner.start();
+//     await logout();
+//   } catch (e) {
+//     console.log('[Profile] logout: ', e.message);
+//   } finally {
+//     dispatch({});
+//     spinner.stop();
+//   }
+// };
 
 //   const _handlePhotoChange = async (url) => {
 //     try {
@@ -51,27 +51,27 @@ const Container = styled.View`
 //   return (
 //     <Container>
 //       <Image showButton rounded />
-//       <Text label="이름" value={name} disabled />
-//       <Text label="이메일" value={email} disabled />
-//       <Button
-//         title="로그아웃"
-//         onPress={() => {
-//           Alert.alert('로그아웃', '로그아웃을 하시려면 확인 버튼을 누르세요.', [
-//             {
-//               text: '취소',
-//               style: 'cancel',
-//             },
-//             {
-//               text: '확인',
-//               onPress: () => {
-//                 setUid('');
-//                 setEmail('');
-//               },
-//             },
-//           ]);
-//         }}
-//         containerStyle={{ marginTop: 30, backgroundColor: theme.buttonLogout }}
-//       />
+// <Text label="이름" value={name} disabled />
+// <Text label="이메일" value={email} disabled />
+// <Button
+//   title="로그아웃"
+//   onPress={() => {
+//     Alert.alert('로그아웃', '로그아웃을 하시려면 확인 버튼을 누르세요.', [
+//       {
+//         text: '취소',
+//         style: 'cancel',
+//       },
+//       {
+//         text: '확인',
+//         onPress: () => {
+//           setUid('');
+//           setEmail('');
+//         },
+//       },
+//     ]);
+//   }}
+//   containerStyle={{ marginTop: 30, backgroundColor: theme.buttonLogout }}
+// />
 //     </Container>
 //   );
 // };
@@ -82,10 +82,51 @@ const Container = styled.View`
 // };
 
 const Profile = () => {
+  const { spinner } = useContext(ProgressContext);
+  const theme = useContext(ThemeContext);
+
+  const [email, setEmail] = useRecoilState(userState.emailState);
+  const [uid, setUid] = useRecoilState(userState.uidState);
+
+  const name = '테스터';
+
+  const _handleLogoutButtonPress = async () => {
+    try {
+      spinner.start();
+      // await logout();
+      setUid('');
+      setEmail('');
+    } catch (e) {
+      console.log('[Profile] logout: ', e.message);
+    } finally {
+      // dispatch({});
+      spinner.stop();
+    }
+  };
+
   return (
-    <View>
-      <Text>핫스팟 구현할 페이지</Text>
-    </View>
+    <Container>
+      <Text>프로필 구현할 페이지</Text>
+      {/* <Image showButton rounded /> */}
+      <Input label="이름" value={name} disabled />
+      <Input label="이메일" value={email} disabled />
+      <Button
+        title="로그아웃"
+        onPress={() => {
+          Alert.alert('로그아웃', '로그아웃을 하시려면 확인 버튼을 누르세요.', [
+            {
+              text: '취소',
+              style: 'cancel',
+            },
+            {
+              text: '확인',
+              onPress: _handleLogoutButtonPress,
+            },
+          ]);
+        }}
+        containerStyle={{ marginTop: 30, backgroundColor: theme.buttonLogout }}
+      />
+    </Container>
   );
 };
 
