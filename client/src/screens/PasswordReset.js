e045d772f9bb8fc1b290baa49360d2567520354b
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-08-31 22:43:43 +0900

    feat : add functions for password resetting
    비밀번호 재설정을 위한 기능(함수, 경고창) 추가

--- client/src/screens/PasswordReset.js
+++ client/src/screens/PasswordReset.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Alert } from 'react-native';
 import styled from 'styled-components/native';
 import axios from 'axios';
@@ -37,6 +37,14 @@ const SubmitContainer = styled.View`
   justify-content: center;
   margin-bottom: 40px;
 `;
+const ErrorText = styled.Text`
+  align-items: flex-start;
+  width: 100%;
+  height: 20px;
+  margin-bottom: 10px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.errorText};
+`;
 const PasswordContainer = styled.View``;
 
 const PasswordReset = ({ navigation }) => {
@@ -47,22 +55,38 @@ const PasswordReset = ({ navigation }) => {
   //TODO : 전송 -> CHECK -> verifyCode입력 -> 이메일, 인증번호 전송, 확인 -> check -> 새 비밀번호 입력 -> 이메일, 새 비밀번호 전송
   const [emailSent, setEmailSent] = useState(false);
   const [verified, setVerified] = useState(false);
+  const [pwdErrorMessage, setPwdErrorMessage] = useState('');
+
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
 
   const insets = useSafeAreaInsets();
 
+  useEffect(() => {
+    if (passwordConfirm && password !== passwordConfirm) {
+      setPwdErrorMessage('비밀번호가 다릅니다. 다시 확인해주세요.');
+    } else {
+      setPwdErrorMessage('');
+    }
+  }, [password, passwordConfirm]);
+
   const sendEmail = async () => {
     try {
-      await axios.post('http://10.0.2.2:3000/account/auth', { email: email }).then((res) => {
-        if (res.status === 'SUCCESS') {
-          setEmailSent(true);
-          Alert.alert('확인코드 전송', '입력하신 이메일로 확인코드를 전송하였습니다. 확인해주세요');
-        } else if (res.status === 'FAILED') {
-          throw new Error(res.data.message);
-        }
-      });
+      await axios
+        .post('http://10.0.2.2:3000/account/auth', { email: email })
+        .then((res) => {
+          if (res.data.status === 'SUCCESS') {
+            setEmailSent(true);
+            Alert.alert('확인코드 전송', '입력하신 이메일로 확인코드를 전송하였습니다. 확인해주세요');
+          } else if (res.data.status === 'FAILED') {
+            // throw new Error(res.data.message);
+            Alert.alert('오류', '해당 이메일로 가입된 정보를 찾을 수 없습니다.');
+          }
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
     } catch (error) {
       Alert.alert('오류', error.message);
     }
@@ -74,12 +98,20 @@ const PasswordReset = ({ navigation }) => {
       await axios
         .post('http://10.0.2.2:3000/account/verifyCode', { email: email, verifyCode: verifyCode })
         .then((res) => {
-          if (res.status === 'SUCCESS') {
+          if (res.data.status === 'SUCCESS') {
+            Alert.alert('인증코드 확인', '인증코드가 확인되었습니다. 새로운 비밀번호를 입력해주세요', [
+              {
+                text: '확인',
+                onPress: () => {
+                  setEmailSent(false);
+                  setVerified(true);
+                },
+              },
+            ]);
+          } else if (res.data.status === 'FAILED') {
+            Alert.alert('오류', '입력코드가 틀렸습니다. 다시 시도해주세요.');
             setEmailSent(false);
-            setVerified(true);
-            Alert.alert('확인코드 전송', '입력하신 이메일로 확인코드를 전송하였습니다. 확인해주세요');
-          } else if (res.status === 'FAILED') {
-            throw new Error(res.data.message);
+            setEmail('');
           }
         });
     } catch (error) {
@@ -87,20 +119,25 @@ const PasswordReset = ({ navigation }) => {
     }
   };
 
-  const setNewPassword = () => {
+  const setNewPassword = async () => {
     //TODO : /account/resetPwd
     try {
-      // await axios
-      //   .post('http://10.0.2.2:3000/account/resetPwd', { email: email, password: passwordConfirm })
-      //   .then((res) => {
-      //     if (res.status === 'SUCCESS') {
-      //       setEmailSent(false);
-      //       setVerified(true);
-      //       Alert.alert('확인코드 전송', '입력하신 이메일로 확인코드를 전송하였습니다. 확인해주세요');
-      //     } else if (res.status === 'FAILED') {
-      //       throw new Error(res.data.message);
-      //     }
-      //   });
+      await axios
+        .post('http://10.0.2.2:3000/account/resetPwd', { email: email, password: passwordConfirm })
+        .then((res) => {
+          if (res.data.status === 'SUCCESS') {
+            Alert.alert('비밀번호 재설정', '비밀번호를 재설정하였습니다. 로그인을 해주세요.', [
+              {
+                text: '확인',
+                onPress: () => {
+                  navigation.navigate('Login');
+                },
+              },
+            ]);
+          } else if (res.data.status === 'FAILED') {
+            throw new Error(res.data.message);
+          }
+        });
     } catch (error) {
       Alert.alert('오류', error.message);
     }
@@ -141,7 +178,7 @@ const PasswordReset = ({ navigation }) => {
               width="250px"
             />
             <Button
-              title="전송"
+              title="확인"
               onPress={() => {
                 sendEmailVerifyCode();
               }}
@@ -159,7 +196,9 @@ const PasswordReset = ({ navigation }) => {
               onChangeText={(text) => {
                 setPassword(text);
               }}
-              onSubmitEditing={() => {}}
+              onSubmitEditing={() => {
+                passwordConfirmRef.current.focus();
+              }}
               placeholder="비밀번호"
               returnKeyType="done"
               isPassword
@@ -172,12 +211,19 @@ const PasswordReset = ({ navigation }) => {
               onChangeText={(text) => {
                 setPasswordConfirm(text);
               }}
-              onSubmitEditing={() => {}}
+              onSubmitEditing={() => {
+                if (passwordConfirm !== password) {
+                  Alert.alert('실패', '비밀번호를 확인해주세요.');
+                } else {
+                  setNewPassword();
+                }
+              }}
               placeholder="비밀번호 확인"
               returnKeyType="done"
               isPassword
               width="300px"
             />
+            <ErrorText>{pwdErrorMessage}</ErrorText>
           </>
         )}
       </FormContainer>
