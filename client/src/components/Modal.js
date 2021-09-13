450393f2df90985b5e53ef2204db399b40af08ac
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-09-13 21:37:05 +0900

    feat : add modal component
    커스텀 모달 컴포넌트 생성 및 민들레 심기 컨텐츠 컴포넌트 생성

---
+++ client/src/components/Modal.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { Modal, Button, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BlackSpace = styled.View`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.View`
+  background-color: #ffffff;
+  width: 300px;
+  height: 250px;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+`;
+const CustomModal = ({ modalVisible, setModalVisible, children }) => {
+  return modalVisible ? (
+    <Modal animationType={'fade'} transparent={true} visible={modalVisible}>
+      <Container>
+        <BlackSpace
+          onTouchEnd={() => {
+            setModalVisible(false);
+          }}
+        >
+          <Content>
+            {children}
+            <Button
+              title="확인"
+              onPress={() => {
+                setModalVisible(false);
+              }}
+            />
+          </Content>
+        </BlackSpace>
+      </Container>
+    </Modal>
+  ) : null;
+};
+
+export default CustomModal;
