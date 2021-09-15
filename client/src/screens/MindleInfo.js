a7bc5e5422e9712eef8a6927ee17cfec5e5bbe70
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-09-15 11:19:35 +0900

    feat : add props for mindle info page
    민들레 속 페이지로 props 전달 추가

--- client/src/screens/MindleInfo.js
+++ client/src/screens/MindleInfo.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 import { View, Text } from 'react-native';
 
 const Container = styled.View`
   flex: 1;
   display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 5px 15px;
+  padding: 15px 15px;
   background-color: #ffffff;
-  height: 610px;
 `;
 
 const StyledText = styled.Text`
@@ -17,11 +14,56 @@ const StyledText = styled.Text`
   font-weight: 600;
 `;
 
-const MindleInfo = () => {
+const Divider = styled.View`
+  margin-top: 10px;
+  height: 1px;
+  border: 0.5px solid #000000;
+`;
+const MindleInfo = ({ navigation, route }) => {
+  const [mindleInfo, setMindleInfo] = useState({ name: '', madeby: '', hashtag: [], visitCount: '', current: '' });
+
+  useEffect(() => {
+    setMindleInfo(route.params.mindleInfo);
+    setTimeout(() => {
+      if (mindleInfo) console.log(mindleInfo);
+    }, 200);
+  }, []);
+
+  const Header = () => {
+    return (
+      <>
+        <View>
+          <View style={{ display: 'flex', flexDirection: 'row', marginBottom: 15 }}>
+            <View style={{ marginRight: 15 }}>
+              <StyledText>{mindleInfo.name}</StyledText>
+            </View>
+            <Text>made by {mindleInfo.madeby}</Text>
+          </View>
+          <View style={{ display: 'flex', flexDirection: 'row', marginTop: 5, marginBottom: 5 }}>
+            <View style={{ marginRight: 15 }}>
+              <Text>누적 방문자 {mindleInfo.visitCount}</Text>
+            </View>
+            <View style={{ marginLeft: 15 }}>
+              <Text>실시간 {mindleInfo.current}</Text>
+            </View>
+          </View>
+          <View style={{ display: 'flex', flexDirection: 'row', marginTop: 5 }}>
+            {mindleInfo.hashtag.map((item, idx) => (
+              <View key={idx} style={{ marginRight: 10 }}>
+                <Text>{item}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+      </>
+    );
+  };
+
   return (
     <>
       <Container>
-        <StyledText>민들레 세부 정보</StyledText>
+        <Header />
+        <Divider />
       </Container>
     </>
   );
