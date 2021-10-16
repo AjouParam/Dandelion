1a6d68565275f608f2be082c97214d7449be2d77
JangByeongHui <jmkabc31@ajou.ac.kr>
2021-10-17 01:52:33 +0900

    feat:add Backend src

---
+++ server/src/models/Comment.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = Schema({
+  _post: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true,
+  },
+  _user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  parentComment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  depth: {
+    type: Number,
+    default: 1,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+  },
+});
+
+const PostComment = mongoose.model('Comment', CommentSchema);
+
+module.exports = PostComment;
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -9,6 +9,14 @@ const UserSchema = new Schema({
     type: Number,
     default: 0,
   },
+<<<<<<< HEAD
+=======
+  thumbnail: String,
+  seeds: {
+    type: Number,
+    default: 0,
+  },
+>>>>>>> 9dea053b16cf54ae3f62e0b09b894af0825e6848
 });
 
 const User = mongoose.model('User', UserSchema);
