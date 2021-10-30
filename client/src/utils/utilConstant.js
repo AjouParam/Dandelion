ea7538660dc38ff4e1a2484ae70897efb53acda3
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-10-30 20:59:32 +0900

    style : modify postMarginHeight
    댓글 입력창 공백 수정을 위해 postMarginHeight을 60에서 45로 수정

--- client/src/utils/utilConstant.js
+++ client/src/utils/utilConstant.js
@@ -1,7 +1,7 @@
 const marginWidth = 80;
 const KeyboardHeight = 250;
 const defaultKeyboardHeight = 0;
-const postMarginHeight = 70;
+const postMarginHeight = 45;
 const defaultMindleImage = 8;
 const marginSwiper = 100;
 
