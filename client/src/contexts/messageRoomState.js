44394034f1346c8ac6fbd563ae106ade4f326031
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-11-12 23:40:41 +0900

    feat-f : add state for message

---
+++ client/src/contexts/messageRoomState.js
@@ -0,0 +1,19 @@
+import { atom } from 'recoil';
+
+const messageRoomState = {
+  roomIdState: atom({
+    key: 'roomIdState',
+    default: '',
+  }),
+  audienceNameState: atom({
+    key: 'audienceNameState',
+    default: '',
+  }),
+
+  lastMessageState: atom({
+    key: 'lastMessageState',
+    default: '',
+  }),
+};
+
+export default messageRoomState;
