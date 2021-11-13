2af319c28ceed41edd11ac1a3e967a00f4cb2314
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-11-14 02:39:49 +0900

    style : add images for app

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -11,5 +11,35 @@ import tester2 from './tester/tester2.png';
 import tester3 from './tester/tester3.png';
 import tester4 from './tester/tester4.png';
 import tester5 from './tester/tester5.png';
+import BackButtonImage from './Button/back_button.svg';
+import CommentImage from './post/comment.png';
+import AddPostImage from './post/post_add.png';
+import DeletePostImage from './post/post_delete.png';
+import PostMenuImage from './post/post_menu.png';
+import PostMenuEditImage from './post/post_menu_edit.png';
+import DefaultProfile from './profile/profile_default.svg';
+import UnlikeImage from './post/like_unclicked.png';
 
-export { logo, button, profile, level1, level2, level3, level4, back, tester1, tester2, tester3, tester4, tester5 };
+export {
+  logo,
+  button,
+  profile,
+  level1,
+  level2,
+  level3,
+  level4,
+  back,
+  tester1,
+  tester2,
+  tester3,
+  tester4,
+  tester5,
+  BackButtonImage,
+  CommentImage,
+  AddPostImage,
+  DeletePostImage,
+  PostMenuImage,
+  PostMenuEditImage,
+  DefaultProfile,
+  UnlikeImage,
+};
