b44d5c71206866b53eb76bf79e9f705c41c3d356
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-11-14 22:14:04 +0900

    feat-f : add creating event function
    이벤트 생성 기능 추가

--- client/src/screens/mindlePage/MakePost.js
+++ client/src/screens/mindlePage/MakePost.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Alert, Dimensions } from 'react-native';
+import { Alert, Dimensions, View, TextInput } from 'react-native';
 import styled from 'styled-components/native';
 import axios from 'axios';
 import { useRecoilValue } from 'recoil';
 import userState from '@contexts/userState';
-
+import DatePicker from 'react-native-date-picker';
 import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 
 const Container = styled.SafeAreaView`
@@ -24,9 +24,46 @@ const TitleContainer = styled.View`
 const TitleInput = styled.TextInput`
   height: 80px;
 `;
-const EventSettings = styled.View``;
+const EventSettings = styled.View`
+  width: 100%;
+  min-height: 30%;
+  margin-bottom: 10px;
+  padding: 10px;
+  background-color: #fbfbfb;
+`;
+const EventDescription = styled.Text`
+  flex-shrink: 1;
+  font-size: 14px;
+  color: #434343;
+  margin: 5px 0px;
+`;
+const EventDateSelect = styled.TouchableOpacity`
+  background-color: #f3d737;
+  width: 120px;
+  height: 24px;
+  justify-content: center;
+  align-items: center;
+`;
+const EventFirstComeInput = styled.TextInput`
+  padding: 0px 10px;
+  margin-left: 5px;
+  background-color: #ffffff;
+  border-radius: 10px;
+`;
+const EventRewardsInput = styled.TextInput`
+  padding: 0px 10px;
+  margin-left: 5px;
+  background-color: #ffffff;
+  border-radius: 10px;
+`;
+
+const FlexRowAlignCenterView = styled.View`
+  flex-direction: row;
+  padding: 0px 5px;
+  align-items: center;
+`;
 const Body = styled.View`
-  height: 80%;
+  flex: 1;
   padding: 15px 5px;
   border-bottom-width: 1px;
   justify-content: space-between;
@@ -76,11 +113,16 @@ const MakePost = ({ navigation, route }) => {
   const bodyTextRef = useRef();
   const submitRef = useRef();
   const [title, setTitle] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
   const [bodyText, setBodyText] = useState('');
   const [images, setImages] = useState([]);
   const [sendImages, setSendImages] = useState([]);
+  const [firstComeNum, setFirstComeNum] = useState(0);
+  const [rewards, setRewards] = useState(0);
   const jwtToken = useRecoilValue(userState.uidState);
   const name = useRecoilValue(userState.nameState);
+
   useEffect(() => {
     axios.defaults.baseURL = 'http://3.35.45.177:3000/';
     axios.defaults.headers.common['x-access-token'] = jwtToken;
@@ -249,7 +291,90 @@ const MakePost = ({ navigation, route }) => {
   };
 
   const setEvent = async () => {
-    await axios.post();
+    const data = {
+      title: title,
+      text: bodyText,
+      location: {
+        longitude: longitude,
+        latitude: latitude,
+      },
+      rewards: rewards,
+      firstComeNum: firstComeNum,
+      startDate: date.toISOString().slice(0, 10),
+    };
+    console.log(data);
+    await axios
+      .post(`/${mindleId}/event/create`, data)
+      .then((res) => {
+        /**{
+          "status": "SUCCESS",
+          "message": "게시글을 작성하였습니다.",
+          data:{ ... },
+          } 
+        */
+        console.log('send');
+        if (res.data.status === 'SUCCESS') {
+          console.log('이벤트');
+          return res.data.data;
+        } else {
+          console.log(res.data);
+          Alert.alert('이벤트 생성 실패', '이벤트 생성에 실패하였습니다. 잠시 후 다시 시도해주세요.');
+          return null;
+        }
+      })
+      .then(async (data) => {
+        if (data && sendImages.length > 0) {
+          console.log(data._id);
+          data.comments = 0;
+          data.likes = 0;
+          console.log(sendImages);
+          let formData = new FormData();
+          sendImages.forEach((item) => formData.append('images', item));
+
+          formData.append('postId', data._id);
+
+          await axios
+            .post(`/dandelion/images/post`, formData, {
+              headers: { 'Content-Type': 'multipart/form-data' },
+            })
+            .then((res) => {
+              if (res.data.status === 'SUCCESS') {
+                console.log(res.data.data.map((item) => item));
+                const userId = data._user;
+                data._user = { _id: userId, name: name };
+                data.images = [
+                  ...res.data.data.map((item) => {
+                    return item.fileUrl;
+                  }),
+                ];
+                console.log(data);
+                //setRefresh(true);
+                //route.params.onGoBack(data);
+                /**
+                 * setEventRefresh(true);
+                 * route.params.onGoEventBack(data);
+                 */
+                navigation.goBack();
+              } else {
+                console.log('이미지 업로드 실패');
+              }
+            })
+            .catch((err) => {
+              console.log('이미지 작성 에러');
+              console.log(err);
+            });
+        } else {
+          console.log('이미지 없음');
+          //setEventRefresh(true);
+          //route.params.onGoEventBack(data);
+          //route.params.onGoBack(data);
+          navigation.goBack();
+        }
+      })
+      .catch((err) => {
+        console.log('이벤트 생성 에러');
+        console.log(err.message);
+      });
   };
 
   const pickImage = () => {
@@ -276,19 +401,80 @@ const MakePost = ({ navigation, route }) => {
 
   return (
     <Container>
-      <TitleContainer>
-        {type === 'Post' && (
-          <TitleInput
-            ref={titleRef}
-            placeholder={'제목'}
-            value={title}
-            onChangeText={(text) => {
-              setTitle(text);
+      {type === 'Event' && (
+        <EventSettings>
+          <EventDescription>
+            • 이벤트를 숨길 장소에서 글을 등록해주세요. 사용자들이 ar을 통해 해당 장소에 접근합니다.
+          </EventDescription>
+          <EventDescription>• 쿠폰은 사진을 통해 등록해주세요. 해당 사진들이 당첨자들에게 전송됩니다.</EventDescription>
+          <View
+            style={{
+              height: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
             }}
-            onSubmitEditing={() => bodyTextRef.current.focus()}
-          ></TitleInput>
-        )}
-        {type === 'Event' && <EventSettings></EventSettings>}
+          >
+            <EventDateSelect
+              onPress={() => {
+                console.log('클릭');
+                setOpen(true);
+              }}
+            >
+              <Text style={{ color: '#fbfbfb' }}>이벤트 시작 날짜</Text>
+            </EventDateSelect>
+            <Text style={{ fontSize: 16, color: '#343434' }}>
+              날짜 : {date.toISOString().slice(0, 10)} / 시간 : {date.toISOString().slice(11, 16)}
+            </Text>
+            <DatePicker
+              modal
+              open={open}
+              date={date}
+              onConfirm={(date) => {
+                setOpen(false);
+                setDate(date);
+              }}
+              onCancel={() => {
+                setOpen(false);
+              }}
+            />
+          </View>
+
+          <FlexRowAlignCenterView styles={{ justifyContent: 'center', width: Dimensions.get('screen').width }}>
+            <FlexRowAlignCenterView>
+              <EventDescription>선착순</EventDescription>
+              <EventFirstComeInput
+                placeholder={'몇명'}
+                value={firstComeNum}
+                onChangeText={(text) => {
+                  setFirstComeNum(text);
+                }}
+              ></EventFirstComeInput>
+            </FlexRowAlignCenterView>
+            <FlexRowAlignCenterView>
+              <EventDescription>리워드</EventDescription>
+              <EventRewardsInput
+                placeholder={'몇개'}
+                value={rewards}
+                onChangeText={(text) => {
+                  setRewards(text);
+                }}
+              ></EventRewardsInput>
+            </FlexRowAlignCenterView>
+          </FlexRowAlignCenterView>
+        </EventSettings>
+      )}
+      <TitleContainer>
+        <TitleInput
+          ref={titleRef}
+          placeholder={'제목'}
+          value={title}
+          onChangeText={(text) => {
+            setTitle(text);
+          }}
+          onSubmitEditing={() => bodyTextRef.current.focus()}
+        ></TitleInput>
       </TitleContainer>
       <Body>
         <BodyInput
@@ -319,10 +505,14 @@ const MakePost = ({ navigation, route }) => {
           <Text
             ref={submitRef}
             onPress={() => {
-              if (modifyMode) {
-                modifyPost();
+              if (type === 'Event') {
+                setEvent();
               } else {
-                setPost();
+                if (modifyMode) {
+                  modifyPost();
+                } else {
+                  setPost();
+                }
               }
             }}
           >
